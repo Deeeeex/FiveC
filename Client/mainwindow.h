4992b3e308eafcfd5df9bc316bd13696e756cbbb
Deeeeex <69737947+Deeeeex@users.noreply.github.com>
2021-08-26 18:53:39 +0800

    Add files via upload

---
+++ Client/mainwindow.h
@@ -0,0 +1,67 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "GameModel.h"
+//#include <QWidget>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QHostAddress>
+/*
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+*/
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget* parent = 0);
+    ~MainWindow();
+
+protected:
+    // 绘制
+    void paintEvent(QPaintEvent* event);
+    // 监听鼠标移动情况，方便落子
+    void mouseMoveEvent(QMouseEvent* event);
+    // 实际落子
+    void mouseReleaseEvent(QMouseEvent* event);
+
+private:
+
+    QTcpSocket *client;
+    QTcpSocket *clientTry;//socket对象
+
+    GameModel* game; // 游戏指针
+    GameType game_type; // 存储游戏类型
+    int clickPosRow, clickPosCol; // 存储将点击的位置
+    int netPosRow=-1, netPosCol=-1;//存储server传来的位置,正常情况下置为(-1,-1)
+    int playerExisted;
+    int Port;
+    //int stackOperation[2][2];//记录前两次操作的栈
+    //int counterFlag;//用于标记下次操作在栈中的位置
+    void initGame();
+    //void checkGame(int y, int x);
+
+
+private slots:
+    void chessOneByPerson(); // 人执行
+    void chessOneByNet();//接受server传来的信息并落子
+    void chessOneByAI(); // AI下棋
+    void chessUndo(int row,int col);
+
+    void initPVPGame();
+    void initPVP_NETGame();
+    void recvMessage();
+    //void initPVP_NETGameGuest();
+    void initPVEGame();
+
+    void Surrender();
+    void Undo();
+
+};
+
+#endif // MAINWINDOW_H
